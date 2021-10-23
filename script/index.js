21c3c3ca163974d8fcde8fa36f1100c6a3f92f8d
anita-brown <anita.osagie@decagon.dev>
2021-10-23 17:59:53 +0100

    add open graph, accesibility and adopt html5

--- script/index.js
+++ script/index.js
@@ -1,7 +1,22 @@
 // Get id from index file and assign to const rootDiv variable
+
 const rootDiv = document.getElementById("root");
 
+
+
+// Declaring a function to display none when document is in block style
+
+function showDetails(id) {
+  const infoDiv = document.getElementById(id);
+  if (infoDiv.style.display == "block") {
+    infoDiv.style.display = "none";
+  } else {
+    infoDiv.style.display = "block";
+  }
+}
+
 //  Assigning images to const imageArr variable
+
 const imageArr = [
   "luke-skywalker.jpeg",
   "C-3PO..jpeg",
@@ -18,35 +33,36 @@ const imageArr = [
 //Assigning output to an empty string
 let output = "";
 
-// fetch API
+// fetch API using .then method
+
 fetch("https://swapi.dev/api/people")
+
   .then((res) => res.json())
   .then((data) => {
-    // Loop through
-    for (let i = 0; i < data.results.length; i++) {
-      output += `
-                    
-                      <div class="character">
-                        <div class="img-box">
-                          <img src='images/${imageArr[i]}' class="char-img" />
-                        </div
-                        <div>
-                          <button type="button" class="btn btn-secondary btn-custom" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" 
-                            data-bs-content="Top popover"onclick="showDetails('myDiv${i}')">
-                            <p>${data.results[i].name}</p>
-                          </button>
-                          <div style='display:none'  id='myDiv${i}' class ="info">
-                            <h4 style='text-align:center'>Gender: ${data.results[i].gender}</h4>
-                            <p  style='text-align:center'>Height: ${data.results[i].height}cm</p>
-                          </div>
-                        </div>
-                      </div>
-                    
-                                 
-          `;
 
+// Loop through array
+
+    for (let i = 0; i < data.results.length; i++) 
+
+    // Inner html display
+    {
+      output += ` 
+       <section class="character">
+          <div class="img-box">
+            <img src='images/${imageArr[i]}' class="char-img" alt="image of ${data.results[i].name}"/>
+          </div>
+          <div>
+            <button type="button" class="btn btn-secondary btn-custom" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" 
+              data-bs-content="Top popover"onclick="showDetails('myDiv${i}')">
+              <p>${data.results[i].name}</p>
+            </button>
+            <div style='display:none'  id='myDiv${i}' class ="info">
+              <h4 style='text-align:center'>Gender: ${data.results[i].gender}</h4>
+              <p  style='text-align:center'>Height: ${data.results[i].height}cm</p>
+            </div>
+       </section>                            
+          `;
         }
-        console.log("--------------------");
         console.log(output);
 
     rootDiv.innerHTML = output;
@@ -54,11 +70,3 @@ fetch("https://swapi.dev/api/people")
 
  
 
-function showDetails(id) {
-  const infoDiv = document.getElementById(id);
-  if (infoDiv.style.display == "block") {
-    infoDiv.style.display = "none";
-  } else {
-    infoDiv.style.display = "block";
-  }
-}
